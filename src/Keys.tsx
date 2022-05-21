b16deed4346861a4a7569b2516bd38282edd9a15
Yaroslav Komarov <yaraslau.komarov@yandex.ru>
2022-05-21 16:08:24 +0500

    complete

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,20 @@
 import { IItem } from './index';
+import { ListItem } from './ListItem';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    props.sorting === 'ASC'
+        ? props.initialData.sort((a, b) => a.id - b.id)
+        : props.initialData.sort((a, b) => b.id - a.id);
+
+    return (
+        <ul>
+            {props.initialData.map((item) => {
+                return (
+                    <li key={item.id}>
+                        <ListItem name={item.name} />
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }
